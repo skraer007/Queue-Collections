adf3148af5a47875fbead92982558830726351cd
Andrey Ivanov <skraer2001@mail.ru>
2022-11-30 19:03:25 +0300

    Version 1.0

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Deque<Person> deque = new ArrayDeque<>(generateClients());
+        while (!deque.isEmpty()) {
+            Person deletedClient = deque.poll();
+            deletedClient.setTickets(1);
+            System.out.println("Клиент " + deletedClient.name + " " + deletedClient.surname +
+                    " прокатился на аттракционе!\nОстаток билетов: " + deletedClient.tickets);
+            if (deletedClient.tickets > 0) {
+                deque.offer(deletedClient);
+            }
+        }
+    }
+
+    public static List<Person> generateClients() {
+        ArrayList<Person> clients = new ArrayList<>();
+        Person person1 = new Person("Anya", "Ivanova", 1);
+        Person person2 = new Person("Tanya", "Smirnova", 2);
+        Person person3 = new Person("Danya", "Vavilov", 3);
+        Person person4 = new Person("Petya", "Sorokin", 4);
+        Person person5 = new Person("Katya", "Kirilova", 5);
+        clients.add(person1);
+        clients.add(person2);
+        clients.add(person3);
+        clients.add(person4);
+        clients.add(person5);
+        return clients;
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+    protected String name;
+    protected String surname;
+    protected int tickets;
+
+    public Person(String name, String surname, int tickets) {
+        this.name = name;
+        this.surname = surname;
+        this.tickets = tickets;
+    }
+
+    public void setTickets(int tickets) {
+        this.tickets -= tickets;
+    }
+}
